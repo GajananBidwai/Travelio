eb611837710bf5252f25afa5806492271bd45d79
Gajanan Bidwai <gajananbidwai525@gmail.com>
2026-01-09 10:45:37 +0530

    Initial Commit

---
+++ Travelio/Travelio/TravelioApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelioApp.swift
+//  Travelio
+//
+//  Created by Neosoft on 09/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TravelioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
